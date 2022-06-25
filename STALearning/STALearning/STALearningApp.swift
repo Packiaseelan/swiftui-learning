c6648c6391f0f0c5b9b886c20ffd8e5ebbd4d6d0
Packiaseelan S <packiaseelan14@gmail.com>
2022-06-25 09:14:48 +0530

    sta learning init

---
+++ STALearning/STALearning/STALearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  STALearningApp.swift
+//  STALearning
+//
+//  Created by Packiaseelan S on 25/06/22.
+//
+
+import SwiftUI
+
+@main
+struct STALearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
