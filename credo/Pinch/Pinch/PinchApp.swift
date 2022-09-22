b5d0b794d6cd57bc8661fabbc186c1476fbc6a8b
Packiaseelan S <packiaseelan14@gmail.com>
2022-09-22 10:52:34 +0530

    pinch app init

---
+++ credo/Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Packiaseelan S on 21/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
