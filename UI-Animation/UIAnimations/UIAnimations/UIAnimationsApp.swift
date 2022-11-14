5a9d3e42286e9fcea71c02cfb4f35eca4a8a8fde
Packiaseelan S <packiaseelan14@gmail.com>
2022-11-14 19:32:51 +0530

    ui animation project init

---
+++ UI-Animation/UIAnimations/UIAnimations/UIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIAnimationsApp.swift
+//  UIAnimations
+//
+//  Created by Packiaseelan S on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct UIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
